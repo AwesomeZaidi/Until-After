e3f5c503fd19f159fbc9c95a145cf88f711d456f
Asim Zaidi <asimzaidih@gmail.com>
2019-01-11 01:55:26 -0800

    working on users seeing who has their code in settings :construction:

--- routes/settings.js
+++ routes/settings.js
@@ -7,18 +7,21 @@ const jwt = require('jsonwebtoken');
 
 /* GET settings page. */
 router.get('/settings', function(req, res, next) {
-    invitecode = req.user.invitecode;
-    
-    if (req.user) {
-        if (invitecode) {
-            User.findById(invitecode).then((friend) => {
+    const invitecode = req.user.invitecode;
+    const friendsIds = req.user.friendsWithPermission;
+    if (req.user) { // check to see if we have a user.
+        const friends = User.find(friendsIds); // find returns a cursor. An array of any objects it finds.
+        console.log("FRIENDS;", friends);
+        
+        if (invitecode) { // check if our user has an accesscode to someones account
+            User.findById(invitecode).then((friend) => { // if so, find who's it is.
                 const name = friend.firstName + " " + friend.firstName;
                 const id = friend._id;
-                const friends = req.user.friendsWithPermission;
+                // next() //read more into what this does exactly, where we go next?           
                 res.render('settings', { name, id, friends });
             })
         } else {
-            res.render('settings');
+            res.render('settings', { friends });
         }
     } else {
         res.redirect('/login');

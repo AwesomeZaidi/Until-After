295e8d2f8209d881d4f8190cdb341637c166aa3e
Asim Zaidi <asimzaidih@gmail.com>
2019-01-16 13:01:34 -0800

    updated env var's..

--- routes/twilio.js
+++ routes/twilio.js
@@ -1,5 +1,5 @@
-const accountSid = "AC6905d00d25382a4e34fbc5811193fa01";
-const authToken = "90da94aa63cf38eb30a4712b79dfe16e";
+const accountSid = process.env.TESTTWILIOSID;
+const authToken = process.env.TESTTWILIOAUTHTOKEN;
 const client = require('twilio')(accountSid, authToken);
 // console.log("client:");
 // console.dir(client);
@@ -8,14 +8,14 @@ module.exports = (function() {
     
     function sendText() {
         console.log("called sendText()");
-        console.log("in function, num:", userNumber);
+        // console.log("in function, num:", userNumber);
         
         // return true;
         client.messages
         .create({
            body: `Hey are you still alive? requested to open your Until After account!`,
            from: '+18474691663',
-           to: '6304077258'
+           to: '+16304077258'
          })
         .then(message => console.log(message.sid))
         .done();
